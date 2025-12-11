2afb507d6c4fba69080fd7c457885952d99d7f68
Noam Zilberstein <noamzilberstein@me.com>
2025-12-11 11:05:23 -0500

    Finish Causality Relation Proof for Guard

--- pcol/Pcol/Semantics/Lpo/Operations/Guard.lean
+++ pcol/Pcol/Semantics/Lpo/Operations/Guard.lean
@@ -22,10 +22,19 @@ noncomputable def guard_base (x : Node) (b : l) (α : Lpo l) (β : Lpo l) : Lpo_
       (β.form y).and (Form.literal x).not
 }
 
+lemma guard_lev {x : Node} {b : l} {α β : Lpo l} :
+    (guard_base x b α β).rel.lev =
+    fun y ↦ if x = y then 0 else if y ∈ α.nodes then α.rel.lev y + 1 else β.rel.lev y + 1 := by
+  ext y; simp only [Rel.lev, guard_base, Set.mem_union]
+  by_cases hx : x = y
+  · simp only [hx, ↓reduceIte]; subst hx; sorry
+  · sorry
+
 lemma guard_rel_valid {x : Node} {b : l} {α : Lpo l} {β : Lpo l}
     (hx₁ : x ∉ α.nodes) (hx₂ : x ∉ β.nodes) (h : Disjoint α.nodes β.nodes) :
     Rel.IsCausalityRel (guard_base x b α β).rel (guard_base x b α β).nodes := by
   constructor
+  -- Transitivity
   · intro y z w
     rintro (hyz | hyz | ⟨rfl, hx | hx⟩) (hzw | hzw | ⟨rfl, hw | hw⟩) <;>
       (try exfalso; exact hx₁ (α.property.rel_dom hyz).2) <;>
@@ -33,18 +42,75 @@ lemma guard_rel_valid {x : Node} {b : l} {α : Lpo l} {β : Lpo l}
       (try exfalso; exact hx₁ hx) <;>
       (try exfalso; exact hx₂ hx)
     · left; exact α.property.rel.trans hyz hzw
-    · sorry -- contradiction
-    · sorry
+    · exfalso; exact Set.disjoint_left.mp h (α.property.rel_dom hyz).2 (β.property.rel_dom hzw).1
+    · exfalso; exact Set.disjoint_left.mp h (α.property.rel_dom hzw).1 (β.property.rel_dom hyz).2
     · right; left; exact β.property.rel.trans hyz hzw
     · right; right; exact ⟨rfl, Or.inl (α.property.rel_dom hzw).2⟩
     · right; right; exact ⟨rfl, Or.inr (β.property.rel_dom hzw).2⟩
     · right; right; exact ⟨rfl, Or.inl (α.property.rel_dom hzw).2⟩
     · right; right; exact ⟨rfl, Or.inr (β.property.rel_dom hzw).2⟩
-  · intro y z; sorry
-  · sorry
-  · sorry
-  · sorry
-  · sorry
+  -- Antisymmetry
+  · intro y z
+    rintro (hyz | hyz | ⟨rfl, hx⟩) (hzy | hzy | ⟨rfl, hy⟩)
+    · exact α.property.rel.antisymm hyz hzy
+    · exfalso
+      exact Set.disjoint_left.mp h (α.property.rel_dom hyz).1 (β.property.rel_dom hzy).2
+    · exfalso; exact hx₁ (α.property.rel_dom hyz).2
+    · exfalso
+      exact Set.disjoint_left.mp h (α.property.rel_dom hzy).1 (β.property.rel_dom hyz).2
+    · exact β.property.rel.antisymm hyz hzy
+    · exfalso; exact hx₂ (β.property.rel_dom hyz).2
+    · exfalso; exact hx₁ (α.property.rel_dom hzy).2
+    · exfalso; exact hx₂ (β.property.rel_dom hzy).2
+    · rfl
+  -- Irreflexitivity
+  · rintro y (hy | hy | ⟨rfl, _ | _⟩) <;> try contradiction
+    · exact α.property.rel.irrefl _ hy
+    · exact β.property.rel.irrefl _ hy
+  -- Well-Foundedness
+  · simp only [guard_base, rel, Set.mem_union]; constructor; intro y
+    induction α.property.rel.wf.apply y with
+    | intro z _ ih₁ =>
+      induction β.property.rel.wf.apply z with
+      | intro w _ ih₂ =>
+        constructor; rintro u (hz | hz | ⟨rfl, _⟩)
+        · exact ih₁ _ hz
+        · apply ih₂ _ hz (fun y _ ↦ α.property.rel.wf.apply y)
+          intro _ hc; exfalso
+          exact Set.disjoint_left.mp h (α.property.rel_dom hc).2 (β.property.rel_dom hz).1
+        · constructor; rintro z (hz | hz | ⟨rfl, _ | _⟩) <;> exfalso <;> try contradiction
+          · exact hx₁ (α.property.rel_dom hz).2
+          · exact hx₂ (β.property.rel_dom hz).2
+  -- Finite Levels
+  · intro n; rw [guard_lev]; simp only [Set.preimage, Set.mem_singleton_iff]
+    by_cases hn : n = 0
+    · subst hn; refine (Set.finite_singleton x).subset ?_
+      simp only [ite_eq_left_iff, Set.subset_singleton_iff, Set.mem_setOf_eq]; intro y hy
+      by_cases hx : x = y
+      · exact hx.symm
+      · apply hy at hx; by_cases hy : y ∈ α.nodes <;>
+          simp only [hy, ↓reduceIte, AddLeftCancelMonoid.add_eq_zero, one_ne_zero, and_false] at hx
+    · refine (Set.Finite.union (α.property.rel.fin_lev (n - 1)) (β.property.rel.fin_lev (n - 1))).subset ?_
+      intro y; simp only [Set.mem_setOf_eq, Set.mem_union, Set.mem_preimage, Set.mem_singleton_iff]
+      intro hy; by_cases hx : x = y <;> simp only [hx, ↓reduceIte] at hy
+      · exfalso; exact hn hy.symm
+      · by_cases hy' : y ∈ α.nodes <;> simp only [hy', ↓reduceIte] at hy
+        · left; rw [← hy, add_tsub_cancel_right]; rfl
+        · right; rw [← hy, add_tsub_cancel_right]; rfl
+  -- Single-Rooted
+  · use x; simp only [Rel.roots, guard_base, Set.mem_union, not_or, not_and]
+    ext y; constructor
+    · simp only [Set.mem_inter_iff, Set.mem_setOf_eq, Set.mem_singleton_iff, and_imp]
+      intro hr hy; rcases Set.eq_or_mem_of_mem_insert hy with (rfl | hy | hy)
+      · rfl
+      · exfalso; exact ((hr x).2.2 rfl).1 hy
+      · exfalso; exact ((hr x).2.2 rfl).2 hy
+    · simp only [Set.mem_singleton_iff, Set.mem_inter_iff, Set.mem_setOf_eq]
+      rintro rfl; refine ⟨fun z ↦ ⟨?_, ?_, ?_⟩, ?_⟩
+      · intro hc; exact hx₁ (α.property.rel_dom hc).2
+      · intro hc; exact hx₂ (β.property.rel_dom hc).2
+      · rintro rfl; exact ⟨hx₁, hx₂⟩
+      · exact Set.mem_insert _ _
 
 noncomputable def guard (x : Node) (b : l) (α : Lpo l) (β : Lpo l)
     (hx₁ : x ∉ α.nodes) (hx₂ : x ∉ β.nodes) (h : Disjoint α.nodes β.nodes)
@@ -86,12 +152,6 @@ noncomputable def guard (x : Node) (b : l) (α : Lpo l) (β : Lpo l)
         · sorry
     · intro y hy; sorry
  )
-
--- lemma guard_monotone_rel {x : Node} {b b' : l} {α α' β β' : Lpo l}
---     (hx₁ : x ∉ α'.nodes) (hx₂ : x ∉ β'.nodes) (hd : Disjoint α'.nodes β'.nodes)
---     (hb : b ≠ ⊥) (hle₁ : α ≤ α') (hle₂ : β ≤ β') (hleb : b ≤ b') :
---   ∀ y ∈ guard x b α β hx₁ hx₂ hd hb, ∀ z ∈ guard x b α β hx₁ hx₂ hd hb,
---     (guard x b α β hx₁ hx₂ hd hb)
 
  lemma guard_monotone {x : Node} {b b' : l} {α α' β β' : Lpo l}
     (hx₁ : x ∉ α'.nodes) (hx₂ : x ∉ β'.nodes) (hd : Disjoint α'.nodes β'.nodes)
